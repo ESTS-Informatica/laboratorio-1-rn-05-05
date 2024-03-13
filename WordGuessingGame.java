b5aaf00e44e78ac47d13a181d817267aa3cc98a5
metalrotiv <162351848+metalrotiv@users.noreply.github.com>
2024-03-13 10:28:19 +0000

    Nivel 2
    erro no metodo play

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -5,12 +5,15 @@
  * @author (Diana e Vitor) 
  * @version (versão 0.1)
  */
+
 public class WordGuessingGame
 {
     // variáveis de instância
     private String hiddenWord;
-    private String guessWord;
-    private int numberOfTries;    
+    private String guessebWord;
+    private int numberOfTries;   
+    private InputReader reader;
+    
 
     /**
      * Construtor para objetos da classe WordGuessingGame
@@ -19,8 +22,9 @@ public class WordGuessingGame
     {
         // inicializa variáveis de instância
         this.hiddenWord = "abc";
-        this.guessWord = "___";
+        this.guessebWord = "___";
         this.numberOfTries = numberOfTries;
+        this.reader = new InputReader();
     }
     
     public void setHiddenWord(String hiddenWord){
@@ -31,12 +35,12 @@ public class WordGuessingGame
         return hiddenWord;
     }
     
-    public void setGuessWord(String guessWord){
-        this.guessWord = guessWord;
+    public void setGuessebWord(String guessebWord){
+        this.guessebWord = guessebWord;
     }
     
-    public String getGuessWord(){
-        return guessWord;
+    public String getGuessebWord(){
+        return guessebWord;
     }
     
     public void setNumberOfTries(int numberOfTries){
@@ -47,7 +51,35 @@ public class WordGuessingGame
         return numberOfTries;
     }
     
-    public void ShowGuessWord(){
-        System.out.println("Guess word: " + getGuessWord());
+    private void showGuessebWord(){
+        System.out.println("Guess word: " + getGuessebWord());
     }
+     public void play(){
+        new InputReader
+        char key = input.next().charAt(0);
+        guess(key);
+        
+        }
+    private void showWelcome(){
+     System.out.println("********* Bem Vindo ********");
+    }
+    
+    private void guess(char letter){
+        for(int i = 0 ; i < hiddenWord.length(); i++ ){ 
+            if(hiddenWord.charAt(i)==letter){
+                System.out.println("Letra encontrada");
+                guessedWord.replace(i, letter);
+                break;
+            }
+            else{
+                System.out.println("Letra não encontrada");
+                numeberOfTries++;
+            }
+        }
+     
+    } 
+    private void showResult(){
+        System.out.println("Tentativas: " + numberOfTries);
+    }
+    
 }
